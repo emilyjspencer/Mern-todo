7c1a37b4f9bb11a74a049577505214aae3252799
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 18:08:48 +0100

    Establishes a connection to the todolist database

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+
+const connection = "mongodb+srv://emily:mongo@cluster0-5zjjm.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(connection, 
+    { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Successfully connected to the todolist database')
+    const db = client.db('todos')
+    const todos = db.collection('todos')
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -9,9 +20,9 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 });
 
-
-
-
 app.listen(5000, function() {
   console.log("Listening for requests on Port 5000")
 })
+
+})
+.catch(console.error)
