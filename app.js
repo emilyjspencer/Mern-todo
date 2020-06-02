027ee5f130b83d8ff9935f902806c560133b31d6
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 17:59:42 +0100

    Installs mongodb and ejs. Creates server file with some middleware. Sets engine to ejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Test')
+});
+
+
+
+
+app.listen(5000, function() {
+  console.log("Listening for requests on Port 5000")
+})
